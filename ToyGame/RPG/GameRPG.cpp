0e0a98aca25008021f6f1d270e5637c563fa9eb8
Daisuke Nishimori <daisukn@outlook.com>
2025-12-05 22:23:18 +0900

    SDL2_mixer切り離し

--- ToyGame/RPG/GameRPG.cpp
+++ ToyGame/RPG/GameRPG.cpp
@@ -4,7 +4,7 @@
 #include "MinionActor.h"
 
 // ToyLibの起動Applicationとして登録
-//TOYLIB_REGISTER_APP(GameRPG)
+TOYLIB_REGISTER_APP(GameRPG)
 
 
 GameRPG::GameRPG()
--- ToyGame/SelectGame.cpp
+++
@@ -1,5 +0,0 @@
-#include "KitRpg.h"
-#include "GameRPG.h"
-#include "Engine/Core/ApplicationEntry.h"
-
-TOYLIB_REGISTER_APP(GameRPG)
